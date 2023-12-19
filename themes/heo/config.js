2556e4bd2cafd9a7dcf2942f4e12a72fecfd85db
liqm <liqm@liqm.com>
2023-12-19 16:11:37 +0800

    config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -12,10 +12,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_3: 'UDETH.COM',
+  HEO_HERO_TITLE_4: '如果遇到困难',
+  HEO_HERO_TITLE_5: '尝试再坚持一次',
+  HEO_HERO_TITLE_LINK: 'https:/udeth.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
